e35950e5569aa0161eb0864e5deef9b4f2adc25a
camilogg15 <camilo.guevarag@gmail.com>
2019-03-20 18:30:29 -0500

    nuevo proyecto

---
+++ 2.reactbasic/src/components/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+    render() { 
+        return ( 
+            <div>
+                Desdse main
+            </div>
+         );
+    }
+}
+export default Main;
